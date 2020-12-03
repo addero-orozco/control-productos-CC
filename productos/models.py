cfcb37a9b84a60a7450b34ab70b714c32ad2e525
Adder Orozco <a.j.orozco038@hotmail.com>
2020-12-02 22:23:10 -0600

    agregado modelo de productos

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+DECIMAL_CERO = Decimal('0.0')
+
+
+class Producto(models.Model):
+    nombre      = models.CharField(
+                            max_length=10,
+                            blank=False,
+                            null=False)
+    precio      = models.DecimalField(
+                            max_digits=8,
+                            decimal_places=2,
+                            default=DECIMAL_CERO)
+    imagen      = models.ImageField(
+                            upload_to='productos_images',
+                            blank=True)
+    descripcion = models.TextField(
+                            default=' ',
+                            blank=True,
+                            null=True)
+    usuario     = models.ForeignKey(
+                            User,
+                            related_name='productos',
+                            null=False,
+                            blank=False,
+                            verbose_name='Usuario',
+                            on_delete=models.CASCADE)
