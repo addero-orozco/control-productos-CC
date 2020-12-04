cbe47a9ba8426f8b57aaee1fa2e7f228eea0750d
Adder Orozco <a.j.orozco038@hotmail.com>
2020-12-03 20:36:59 -0600

    cambios en frontend y urls

--- control/urls.py
+++ control/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('', include('ventas.urls')),
     path('api/v1/', include('api_v1.urls')),
 ]
